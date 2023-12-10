da5f957315ad0b711fe6f6cc3a26a00f180ce497
Tasin Tausif <tasintausif@gmail.com>
2023-12-10 11:41:42 +0600

    Logo and options changed

--- resources/views/sales/saleactivities/create.blade.php
+++ resources/views/sales/saleactivities/create.blade.php
@@ -60,7 +60,7 @@
                 <datalist id="activity_type_list">
                     <option value="Physical Visit"></option>
                     <option value="Telephone Conversation"></option>
-                    <option value="Ranks Office Visit"></option>
+                    <option value="Jumairah Office Visit"></option>
                 </datalist>
             </div>
         </div>
@@ -104,4 +104,3 @@
         });//document.ready
     </script>
 @endsection
-
