c643bf7d4f8df1e5014633ac73cc294a12df4fbd
Monayem Hosen Rahi <monayemhosen02@gmail.com>
2023-12-12 14:17:49 +0600

    salary process working

--- Modules/HR/Http/Controllers/ProcessedSalaryController.php
+++ Modules/HR/Http/Controllers/ProcessedSalaryController.php
@@ -3,6 +3,7 @@
 namespace Modules\HR\Http\Controllers;
 
 use Carbon\Carbon;
+use App\CostCenter;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
@@ -11,10 +12,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\QueryException;
 use Modules\Accounting\Entities\Account;
 use Modules\HR\Entities\ProcessedSalary;
-use Modules\Accounting\Entities\CostCenter;
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Modules\Accounting\Entities\Transaction;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ProcessedSalaryController extends Controller
 {
@@ -31,8 +31,7 @@ class ProcessedSalaryController extends Controller
         $cost_center_id = $request->filter_cost_center_id;
 
         $processed_salaries =
-            ProcessedSalary::where('com_id', auth()->user()->com_id)
-            ->when($month, function ($query) use ($month) {
+            ProcessedSalary::when($month, function ($query) use ($month) {
                 return $query->where('month', $month);
             })
             ->when($department_id, function ($query) use ($department_id) {
@@ -45,8 +44,8 @@ class ProcessedSalaryController extends Controller
             ->paginate($request->per_page ?? 15)
             ->withQueryString();
 
-        $departments = Department::where('com_id', auth()->user()->com_id)->where('status', 'Active')->pluck('name', 'id');
-        $costCenters = CostCenter::where('com_id', auth()->user()->com_id)->pluck('name', 'id');
+        $departments = Department::orderBy('name')->pluck('name', 'id');
+        $costCenters = CostCenter::orderBy('name')->pluck('name', 'id');
         return view('hr::salary.index_processed', compact('processed_salaries', 'departments', 'costCenters'));
     }
 
@@ -54,23 +53,21 @@ class ProcessedSalaryController extends Controller
     {
         $this->authorize('process-salary');
 
-        $com_user_id = Auth::user()->com_id;
+        // $com_user_id = Auth::user()->com_id;
         $cost_center_id = $request->cost_center_id;
         $department_id = $request->department_id;
         $month = $request->month;
 
         try {
             DB::select(
-                "SET @month = ?, @Department = ?, @Com_User = ?, @CostCenter = ?",
-                [$month, $department_id, $com_user_id,  $cost_center_id]
+                "SET @month = ?, @Department = ?, @CostCenter = ?",
+                [$month, $department_id, $cost_center_id]
             );
-            DB::select("CALL salary_processed(@month,@Department,@Com_User,@CostCenter)");
+            DB::select("CALL salary_processed(@month,@Department,@CostCenter)");
 
-            $AllProcessedSalary = ProcessedSalary::where('com_id', $com_user_id)
-                ->where('month', $month)
+            $AllProcessedSalary = ProcessedSalary::where('month', $month)
                 ->where('department_id', $department_id)
                 ->where('cost_center_id', $cost_center_id)
-                ->where('com_id', $com_user_id)
                 ->get();
 
             // dd($AllProcessedSalary);
@@ -103,48 +100,48 @@ class ProcessedSalaryController extends Controller
 
 
 
-            if (count($AllProcessedSalary) > 0) {
+            // if (count($AllProcessedSalary) > 0) {
 
-                $transactionData = [
-                    'voucher_type' => 'Journal',
-                    'transactionable_type' => ProcessedSalary::class,
-                    'transactionable_id' => $AllProcessedSalary[0]->id,
-                    'bill_no' => null,
-                    'mr_no' => null,
-                    'transaction_date' => Carbon::now()->format('d-m-Y'),
-                    'narration' => 'salary',
-                    'user_id' => auth()->id(),
-                ];
+            //     $transactionData = [
+            //         'voucher_type' => 'Journal',
+            //         'transactionable_type' => ProcessedSalary::class,
+            //         'transactionable_id' => $AllProcessedSalary[0]->id,
+            //         'bill_no' => null,
+            //         'mr_no' => null,
+            //         'transaction_date' => Carbon::now()->format('d-m-Y'),
+            //         'narration' => 'salary',
+            //         'user_id' => auth()->id(),
+            //     ];
 
-                $transaction = Transaction::create($transactionData);
+            //     $transaction = Transaction::create($transactionData);
 
-                // ledger entry
+            //     // ledger entry
 
-                $SalaryAllowanceAndBenefits = Account::where('com_id', $com_user_id)->where('balance_and_income_line_id', 86)->where('account_name', 'Salary, Allowances & Benefits')->first();
+            //     $SalaryAllowanceAndBenefits = Account::where('com_id', $com_user_id)->where('balance_and_income_line_id', 86)->where('account_name', 'Salary, Allowances & Benefits')->first();
 
-                if ($cost_center_id == 1 || $cost_center_id == 3) {
-                    $AdminSalaryLedgerData = [
-                        'account_id' => $SalaryAllowanceAndBenefits->id,
-                        'dr_amount' => $total_salary,
-                        'cost_center_id' => $cost_center_id,
-                        'remarks' => 'salary',
-                        'purpose' => 'administrative salary expense',
-                        'balance_and_income_line_id' => $SalaryAllowanceAndBenefits->balance_and_income_line_id,
-                    ];
-                    $transaction->ledgerEntries()->create($AdminSalaryLedgerData);
-                } else {
-                    $FactorySalaryAllowanceAccount = Account::where('com_id', $com_user_id)->where('balance_and_income_line_id', 168)->where('account_name', 'Factory - Salary & Allowance')->first();
-                    $FactorySalaryLedgerData = [
-                        'account_id' => $FactorySalaryAllowanceAccount->id,
-                        'dr_amount' => $total_salary,
-                        'cost_center_id' => $cost_center_id,
-                        'remarks' => 'salary',
-                        'purpose' => 'factory salary expense',
-                        'balance_and_income_line_id' =>  $FactorySalaryAllowanceAccount->balance_and_income_line_id,
-                    ];
-                    $transaction->ledgerEntries()->create($FactorySalaryLedgerData);
-                }
-            }
+            //     if ($cost_center_id == 1 || $cost_center_id == 3) {
+            //         $AdminSalaryLedgerData = [
+            //             'account_id' => $SalaryAllowanceAndBenefits->id,
+            //             'dr_amount' => $total_salary,
+            //             'cost_center_id' => $cost_center_id,
+            //             'remarks' => 'salary',
+            //             'purpose' => 'administrative salary expense',
+            //             'balance_and_income_line_id' => $SalaryAllowanceAndBenefits->balance_and_income_line_id,
+            //         ];
+            //         $transaction->ledgerEntries()->create($AdminSalaryLedgerData);
+            //     } else {
+            //         $FactorySalaryAllowanceAccount = Account::where('com_id', $com_user_id)->where('balance_and_income_line_id', 168)->where('account_name', 'Factory - Salary & Allowance')->first();
+            //         $FactorySalaryLedgerData = [
+            //             'account_id' => $FactorySalaryAllowanceAccount->id,
+            //             'dr_amount' => $total_salary,
+            //             'cost_center_id' => $cost_center_id,
+            //             'remarks' => 'salary',
+            //             'purpose' => 'factory salary expense',
+            //             'balance_and_income_line_id' =>  $FactorySalaryAllowanceAccount->balance_and_income_line_id,
+            //         ];
+            //         $transaction->ledgerEntries()->create($FactorySalaryLedgerData);
+            //     }
+            // }
 
             return redirect()->route(
                 'process-salary.index',
